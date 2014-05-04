19a7afcea84275bf535c20c734ce4f99988c8894
3rd-Eden <info@3rd-Eden.com>
2014-05-04 18:02:50 -0400

    [minor] minimal docs listing

---
+++ pages/docs.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//
+// Extend the default page.
+//
+require('../base').Page.extend({
+  path: '/docs',                  // HTTP route we should respond to.
+  view: '../views/docs.ejs',      // The base template we need to render.
+
+  pagelets: {                     // The pagelets that should be rendered.
+    navigation: require('../pagelets/navigation'),
+    sidebar: require('npm-documentation-pagelet').sidebar,
+    footer: require('../contour').footer
+  }
+}).on(module);
