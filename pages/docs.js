f3ad257d4d7d193310e4a5b4027b126366104bd9
3rd-Eden <info@3rd-Eden.com>
2014-05-04 18:05:31 -0400

    [fix] Use the correct URL for the documentation

--- pages/docs.js
+++ pages/docs.js
@@ -4,7 +4,7 @@
 // Extend the default page.
 //
 require('../base').Page.extend({
-  path: '/docs',                  // HTTP route we should respond to.
+  path: '/help',                  // HTTP route we should respond to.
   view: '../views/docs.ejs',      // The base template we need to render.
 
   pagelets: {                     // The pagelets that should be rendered.
