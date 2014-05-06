e7329ad40cdee41bc7002b3c9f71125d650f3b28
Swaagie <info@martijnswaagman.nl>
2014-05-06 19:24:41 +0200

    [fix] send a lot less data

--- pagelets/status/index.js
+++ pagelets/status/index.js
@@ -6,11 +6,26 @@ var pagelet = require('registry-status-pagelet');
 // Extend the registry status pagelet.
 //
 module.exports = pagelet.extend({
+
   //
   // Reference to the data collection of npm-probe.
   //
   get status() {
-    return this.pipe['npm-probe'].data || {};
+    var results = {}
+      , pagelet = this
+      , data = this.pipe['npm-probe'].data;
+
+    for (var type in data) {
+      results[type] = data[type] || {};
+
+      for (var registry in data[type]) {
+        results[type][registry] = data[type][registry].slice(
+          pagelet.options[type].n
+        );
+      }
+    }
+
+    return results;
   },
 
   //
