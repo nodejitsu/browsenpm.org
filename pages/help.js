a3eb675512559ad506185758cb38680fe416b17e
Swaagie <info@martijnswaagman.nl>
2014-05-09 13:04:40 +0200

    [fix] output help in grid

--- pages/help.js
+++ pages/help.js
@@ -4,7 +4,7 @@
 // Extend the default page.
 //
 require('../base').Page.extend({
-  path: '/docs/:category/:name',  // HTTP route we should respond to.
+  path: '/help/:category/:name',  // HTTP route we should respond to.
   view: '../views/help.ejs',      // The base template we need to render.
 
   pagelets: {                     // The pagelets that should be rendered.
