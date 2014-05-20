d28403878d689eb49765f3249f062cc256df65d5
3rd-Eden <info@3rd-Eden.com>
2014-05-20 17:12:29 +0200

    [fix] Added missing title tag. Fixes #27

--- pages/package.json.js
+++ pages/package.json.js
@@ -9,6 +9,10 @@ base.Page.extend({
   path: '/package.json',
   view: '../views/package.json.ejs',
 
+  data: {
+    title: 'package.json: an interactive guide - browsenpm.org'
+  },
+
   pagelets: base.pagelets.add({
     'package.json': require('npm-package-json-pagelet')
   })
