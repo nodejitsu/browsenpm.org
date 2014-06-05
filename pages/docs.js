cbd80ad8acead78559c265341ffb2f50c19ab689
Swaagie <info@martijnswaagman.nl>
2014-06-05 19:29:16 +0200

    [minor] add npm cheat sheet content, still needs styling

--- pages/docs.js
+++ pages/docs.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var base = require('../base');
-
+var base = require('../base')
+  , contour = require('./contour')
+  , npm = contour.get('npm');
 //
 // Extend the default page.
 //
 base.Page.extend({
   path: '/help',
   view: '../views/docs.ejs',
+
+  dependencies: [
+    npm.get('code')
+  ],
 
   pagelets: base.pagelets.add({
     toc: require('../pagelets/toc'),
