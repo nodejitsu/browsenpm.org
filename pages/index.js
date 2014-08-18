84afe7d52ad03e0a97fb75054320343ae712ecb4
Swaagie <info@martijnswaagman.nl>
2014-08-18 14:29:51 +0200

    [minor] prepare for explore pagelet

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 'use strict';
 
-var base = require('../base');
+var base = require('../base')
+  , path = require('path');
 
 //
 // Extend the default page.
 //
-base.Page.extend({
+module.exports = base.Page.extend({
   path: '/',
-  view: '../views/index.ejs',
+  view: path.join(__dirname, '..', 'views/index.ejs'),
+
+  //
+  // Add style to explore title and subtitle.
+  //
+  dependencies: base.Page.prototype.dependencies.concat([
+    path.join(__dirname, '..', 'assets/css/explore.styl')
+  ]),
 
   pagelets: base.pagelets.add({
-    search: require('npm-search-pagelet')
+    search: require('npm-search-pagelet'),
+    explore: require('cascading-grid-pagelet')
   })
-}).on(module);
+});
