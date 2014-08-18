bd7831fc76ebb6f3c36fd67d8f4959ce517ecb93
Swaagie <info@martijnswaagman.nl>
2014-08-18 15:58:30 +0200

    [minor] provide data and two blocks to cascading grid

--- pagelets/explore/index.js
+++ pagelets/explore/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
-var cascade = require('cascading-grid-pagelet');
+var cascade = require('cascading-grid-pagelet')
+  , frameworks = require('../meta/frameworks')
+  , testing = require('../meta/testing');
 
 //
 // Provide data to the Cascading Grid Pagelet.
 //
 module.exports = cascade.extend({
-  blocks: {[
-
+  blocks: [{
+    title: 'Web frameworks',
+    sub: 'Popular Node.JS frameworks',
+    link: '/explore/frameworks',
+    height: 4,
+    width: 7,
+    hover: {
+      modules: tests.length
+      contributors: 12 // TODO: Calculate number from module/CouchDB data
+    }
+  }, {
+    title: 'Testing',
+    sub: 'Modules useful for test driven development',
+    link: '/explore/testing',
+    height: 4,
+    width: 7,
+    hover: {
+      modules: tests.length
+      contributors: 12 // TODO: Calculate number from module/CouchDB data
+    }
   }]
 });
