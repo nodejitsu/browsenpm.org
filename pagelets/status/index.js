dad9d1533a8e4cffe413137fa299593b588b7d15
Swaagie <info@martijnswaagman.nl>
2015-01-05 17:23:52 +0100

    [fix] remove circular reference from status pagelet

--- pagelets/status/index.js
+++ pagelets/status/index.js
@@ -10,6 +10,34 @@ base.Pagelet.extend({
   view: 'base.ejs',
 
   pagelets: base.pagelets.add({
-    status: require('../status'),
+    status: require('registry-status-pagelet').extend({
+      //
+      // Reference to the data collection of npm-probe.
+      //
+      get status() {
+        var results = {}
+          , pagelet = this
+          , data = this._pipe['npm-probe'].data;
+
+        for (var type in data) {
+          results[type] = data[type] || {};
+
+          for (var registry in data[type]) {
+            results[type][registry] = data[type][registry].slice(
+              data[type][registry].length - pagelet.options[type].n - 1
+            );
+          }
+        }
+
+        return results;
+      },
+
+      //
+      // Reference to latest measurement data.
+      //
+      get latest() {
+        return this._pipe['npm-probe'].latest || {};
+      }
+    })
   })
 }).on(module);
