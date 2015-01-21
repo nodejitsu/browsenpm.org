534bcb6c1d470e59c3d83789705de727f3080ab2
Swaagie <info@martijnswaagman.nl>
2015-01-21 17:31:29 +0100

    [fix] neat layout for 404/500

--- pagelets/500/index.js
+++ pagelets/500/index.js
@@ -1,9 +1,32 @@
 'use strict';
 
+var contour = require('../contour')
+  , npm = contour.get('npm');
+
 //
 // Extend the default page.
 //
-require('../base').Pagelet.extend({
-  path: '/500',
-  view: './base.ejs'
+require('500-pagelet').extend({
+  view: './base.ejs',
+  pagelets: {
+    navigation: require('../navigation'),
+    footer: contour.footer,
+    analytics: contour.analytics.extend({
+      data: {
+        domain: 'browsenpm.org',
+        key: 'gikqh9ctxn',
+        type: 'segment'
+      }
+    })
+  },
+
+  dependencies: [
+    npm.normalize,
+    npm.global,
+    npm.grid,
+    npm.icons,
+    npm.typography,
+    npm.animations,
+    npm.tables
+  ]
 }).on(module);
